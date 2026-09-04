2c1c4496b14f84b6335e3b3a92b0827bca79d565
Cauã <cauagdave4@gmail.com>
2026-09-04 18:59:58 -0300

    limpar

--- Calculadora de IMC.py
+++
@@ -1,4 +0,0 @@
-peso = float(input("Qual o seu peso: "))
-altura = float(input("Qual a sua altura: "))
-imc = peso / (altura * altura)
-print(f"O seu IMC é de {imc}")
--- README.md
+++
@@ -1,1 +0,0 @@
-comecei aprender python dia 30/08/2026, pretendo compartilhar tudo que vou apredendo. 
